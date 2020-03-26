9a2c073e66f18766a161ae86f8615d23aecd4f7b
Naegin <hellonaegin@gmail.com>
2020-03-25 23:57:09 -0300

    Bump Copyright year.

--- kitsu/__init__.py
+++ kitsu/__init__.py
@@ -3,7 +3,7 @@
 __title__ = 'kitsu-wrapper'
 __author__ = 'Naegin'
 __license__ = 'MIT'
-__copyright__ = 'Copyright (c) 2019 Naegin'
+__copyright__ = 'Copyright (c) 2019-2020 Naegin'
 __version__ = '0.1.2'
 
 from .client import Client
