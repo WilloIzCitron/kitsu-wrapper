c736f374ed2353c5bb91f587fb1faca43a68fc3f
Naegin <hellonaegin@gmail.com>
2020-04-16 00:50:21 -0300

    Bump version to '1.0.0'.

--- kitsu/__init__.py
+++ kitsu/__init__.py
@@ -4,6 +4,6 @@ __title__ = 'kitsu-wrapper'
 __author__ = 'Naegin'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2019-2020 Naegin'
-__version__ = '0.1.6'
+__version__ = '1.0.0'
 
 from .client import Client
